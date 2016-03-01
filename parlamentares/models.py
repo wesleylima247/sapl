24c140f4ed6fea13e824db72f73ffca6fae4e300
Marcio Mazza <mazza@interlegis.leg.br>
2016-03-01 17:23:01 -0300

    Adiciona validador de tipo de texto a model

--- parlamentares/models.py
+++ parlamentares/models.py
@@ -3,7 +3,8 @@ import datetime
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
-from sapl.utils import YES_NO_CHOICES, make_choices
+from sapl.utils import (YES_NO_CHOICES, make_choices,
+                        restringe_tipos_de_arquivo_img)
 
 
 class Legislatura(models.Model):
@@ -271,7 +272,8 @@ class Parlamentar(models.Model):
         blank=True,
         null=True,
         upload_to=foto_upload_path,
-        verbose_name=_('Fotografia'))
+        verbose_name=_('Fotografia'),
+        validators=[restringe_tipos_de_arquivo_img])
 
     class Meta:
         verbose_name = _('Parlamentar')
